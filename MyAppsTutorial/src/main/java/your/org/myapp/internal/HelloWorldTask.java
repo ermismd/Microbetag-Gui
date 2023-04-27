c57988e5d3871dd0c826d28b5d27ea1ed18c5bb1
ermismd <ermismdelopoulos@outlook.com>
2023-04-27 15:14:06 +0200

    Step 3...... Tutorial

--- MyAppsTutorial/src/main/java/your/org/myapp/internal/HelloWorldTask.java
+++ MyAppsTutorial/src/main/java/your/org/myapp/internal/HelloWorldTask.java
@@ -1,9 +1,13 @@
 package your.org.myapp.internal;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
@@ -17,46 +21,65 @@ public class HelloWorldTask extends AbstractTask {
 	
 	
 	
-	public HelloWorldTask (final CyNetworkManager helloworldtaskmanager, final CyNetworkNaming nameU, final CyNetworkFactory helloworldtaskfactory) {
+		public HelloWorldTask (final CyNetworkManager helloworldtaskmanager, 
+							   final CyNetworkNaming nameU, 
+							   final CyNetworkFactory helloworldtaskfactory) {
 		
-		this.helloworldtaskmanager = helloworldtaskmanager;
-		this.helloworldtaskfactory = helloworldtaskfactory;
-		this.nameU = nameU;
-	}
+				this.helloworldtaskmanager = helloworldtaskmanager;
+				this.helloworldtaskfactory = helloworldtaskfactory;
+				this.nameU = nameU;
+		}
 		
 
 	
-	public void run(TaskMonitor monitor) throws Exception {
-		// Create an empty network
-			CyNetwork myNet = helloworldtaskfactory.createNetwork();
-			myNet.getRow(myNet).set(CyNetwork.NAME, nameU.getSuggestedNetworkTitle("Hello World Network"));
+		public void run(TaskMonitor monitor) throws Exception {
+			// Create an empty network
+				CyNetwork myNet = helloworldtaskfactory.createNetwork();
+				myNet.getRow(myNet).set(CyNetwork.NAME, nameU.getSuggestedNetworkTitle("Hello World Network"));
 		
 		
-		// Add two nodes to the network
-			CyNode node1 = myNet.addNode();
-			CyNode node2 = myNet.addNode();
+			// Add two nodes to the network
+				CyNode node1 = myNet.addNode();
+				CyNode node2 = myNet.addNode();
 				
-		// set name for new nodes
+			// set name for new nodes
 			
-			CyTable nodeTable = myNet.getDefaultNodeTable(); // Added CyTable nodeTable = myNet.getDefaultNodeTable(); to get the node table for the network.
+				CyTable Table = myNet.getDefaultNodeTable(); // Added CyTable nodeTable = myNet.getDefaultNodeTable(); 
+																//to get the node table for the network.
 			
-			myNet.getDefaultNodeTable().getRow(node1.getSUID()).set("name", "Node1");
-			myNet.getDefaultNodeTable().getRow(node2.getSUID()).set("name", "Node2");	
+				myNet.getDefaultNodeTable().getRow(node1.getSUID()).set("name", "Node1");
+				myNet.getDefaultNodeTable().getRow(node2.getSUID()).set("name", "Node2");	
 
-		// Add an edge
-			myNet.addEdge(node1, node2, true);
+			// To create two new node columns, you can use the following code
+				Table.createColumn("Greeting", String.class, false);
+				Table.createColumn("Value", Double.class, false);
+
+			 //To add data to the new columns, you can use the following code
+				
+				CyRow row1 = Table.getRow(node1.getSUID());
+				CyRow row2 = Table.getRow(node2.getSUID());
+
+				row1.set("Greeting", new String("Hello"));    //creates a list of strings for the "Greeting" column. 
+				row1.set("Value", 1.0);                          //and adds a double value to the "Value" column for both nodes.
+			
+				row2.set("Greeting", new String("Geia") );
+				row2.set("Value", 2.0);
+
+			// Add an edge
+				myNet.addEdge(node1, node2, true);
 			
 			
 			
 			
-			helloworldtaskmanager.addNetwork(myNet);
+				helloworldtaskmanager.addNetwork(myNet);
 			
-		// Set the variable destroyNetwork to true, the following code will destroy a network
-			boolean destroyNetwork = false;
-			if (destroyNetwork){
-				// Destroy it
-				helloworldtaskmanager.destroyNetwork(myNet);			
+			// Set the variable destroyNetwork to true, the following code will destroy a network
+				boolean destroyNetwork = false;
+				if (destroyNetwork){
+
+					// Destroy it
+					helloworldtaskmanager.destroyNetwork(myNet);			
 			
-		}					
-	}
+				}					
+		}
 }
--- MyAppsTutorial/src/main/java/your/org/myapp/internal/HelloWorldTaskFactory.java
+++ MyAppsTutorial/src/main/java/your/org/myapp/internal/HelloWorldTaskFactory.java
@@ -14,19 +14,22 @@ public class HelloWorldTaskFactory extends AbstractTaskFactory {
 	private final CyNetworkFactory helloworldtaskfactory;
 	private final CyNetworkNaming nameU; 
 	
-	public HelloWorldTaskFactory(final CyNetworkManager helloworldtaskmanager, final CyNetworkNaming nameU, final CyNetworkFactory helloworldtaskfactory) {
-		this.helloworldtaskmanager = helloworldtaskmanager;
-		this.nameU = nameU;
-		this.helloworldtaskfactory = helloworldtaskfactory;
-	}
+	    public HelloWorldTaskFactory(final CyNetworkManager helloworldtaskmanager, 
+                                     final CyNetworkNaming nameU, 
+                                     final CyNetworkFactory helloworldtaskfactory) {
+            this.helloworldtaskmanager = helloworldtaskmanager;
+		    this.nameU = nameU;
+		    this.helloworldtaskfactory = helloworldtaskfactory;
+	    }
 	
 
-    public TaskIterator createTaskIterator() {
-        return new TaskIterator(new HelloWorldTask(helloworldtaskmanager, nameU, helloworldtaskfactory)); // Fill in task iterator here
-    }
+        public TaskIterator createTaskIterator() {
+            return new TaskIterator(new HelloWorldTask(helloworldtaskmanager, nameU, helloworldtaskfactory)); // Fill in task iterator here
+        }
 
-    public boolean isReady() {
-        return true;
-    }
+        public boolean isReady() {
+            // Return whether the factory is ready to create tasks or not
+            return true;
+        }
 }
     
